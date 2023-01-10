d0ad4b3be3bb921b474563fb6a733c1520df9774
FireXtz <phantomrising@protonmail.com>
2023-01-09 23:06:54 -0300

    pages index.tsx modificado agenda criada estrutura inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,42 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-     <main className={styles.container}>
+      <main className={styles.container}>
 
-      <form action="">
-        <input type="text" placeholder='Nome'/>
-        <input type="text" placeholder='Email'/>
-        <input type="text" placeholder='Telefone'/>
-        <textarea placeholder='Observações'></textarea>
-        <button type='submit'>Salvar</button>
-      </form>
+        <form action="">
+          <input type="text" placeholder='Nome' />
+          <input type="text" placeholder='Email' />
+          <input type="text" placeholder='Telefone' />
+          <textarea placeholder='Observações'></textarea>
+          <button type='submit'>Salvar</button>
+        </form>
 
-      <div>
-        <input type="text" placeholder='Buscar' />
-      </div>
+        <div className={styles.caixacontatos}>
+          <input type="text" placeholder='Buscar' />
 
-     </main>
+          <div className={styles.caixaUnico}>
+
+
+            <div className={styles.boxTitulo}>
+              <p className={styles.nomeTitulo}>Fulano de tal</p>
+            </div>
+
+            <div>
+              <a>Editar</a>
+              <a>Remover</a>
+            </div>
+
+            <div className={styles.dados}>
+              <p>fulanodeTal@gmail.com</p>
+              <p>0000010221</p>
+              <p>Fulano de tal da rua tal</p>
+            </div>
+
+          </div>
+
+        </div>
+
+      </main>
     </>
   )
 }
